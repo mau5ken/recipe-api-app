57c85f3e5f0da4e68dcb21fe91d4296b9b6ff821
ken <harygato3@gmail.com>
2019-12-22 03:18:46 -0600

    Added states, refactored code with and array of objects and pull info of hits

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import './App.css';
 
 const App = () => {
   
+  //API de Edamam
   const APP_ID = 'aba9d0e3';
   const APP_KEY = 'cc403e21ac05575b1e71cc585661d9ac';
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  //Las recetas quedan dentro de este estado
+  const [recipes, setRecipes] = useState([]); 
 
   useEffect(() => {
-    console.log('effect has been run');
-  });
+    getRecipes();
+  }, []);
   
+  //Obtenemos el request y hacemos fetch a la petición
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    console.log(data);
+    //Se extraen los hits del request, que es donde se provee info como: Ingredientes, calorías porciones etc.
+    setRecipes(data.hits);
+  }
+
   return(
     <div className="App">
       <form className="search-form">
