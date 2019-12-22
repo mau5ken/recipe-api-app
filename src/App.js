509bb198e8c6b4b334339feb582bed602aa82509
ken <harygato3@gmail.com>
2019-12-22 03:00:12 -0600

    first commit with changes on HTML react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+  
+  const APP_ID = 'aba9d0e3';
+  const APP_KEY = 'cc403e21ac05575b1e71cc585661d9ac';
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    console.log('effect has been run');
+  });
+  
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">search</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
